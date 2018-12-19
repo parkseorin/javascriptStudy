7d20b3bd11f1f0bdd8221de8fea364f099ebf065
parkseorin <39305838+parkseorin@users.noreply.github.com>
2018-12-19 23:05:15 +0900

    18.12.19 생활코딩 로그인 조건문 연습
    18.12.19 생활코딩 로그인 조건문 연습 (&&)

---
+++ 생활코딩/조건문 && 로그인 연습.js
@@ -0,0 +1,7 @@
+var id = prompt("아이디를 입력하세요.");
+var pw = prompt("비밀번호를 입력하세요.");
+if ( id == 'skygduanr' && pw == '111111') {
+	alert("로그인에 성공하였습니다.")
+} else {
+	alert("아이디 또는 비밀번호가 일치 하지 않습니다.")
+}
